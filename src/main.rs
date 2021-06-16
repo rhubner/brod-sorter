db3ea067467ba79347628307074f397bed3c4f88
Radek Hubner <rhubner@users.noreply.github.com>
2021-06-16 17:47:27 +0100

    Read only jpeg metadata.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::path::Path;
+
+#[derive(Debug)]
 pub enum Stanice {
     BROD,
     STAVIDLA
@@ -11,9 +14,30 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    for entry in WalkDir::new(&args[1]).into_iter().filter_map(|e| e.ok()) {
-        show_focal_length(entry.path().to_str().unwrap());
-    }
+    for entry in WalkDir::new(&args[1]).into_iter().filter_map(|e| e.ok())
+         .filter(|x| x.path().to_str().unwrap().ends_with("jpg") ) {
+         read_jpeg(&entry.path());
+     }
+}
+
+fn read_jpeg(path: &Path) {
+    use std::fs::File;
+    use std::io::BufReader;
+    use jpeg_decoder::Decoder;
+
+    let file = File::open(path).expect("failed to open file");
+    let mut decoder = Decoder::new(BufReader::new(file));
+    decoder.read_info();
+    let metadata = decoder.info().unwrap();
+
+    let x = if (metadata.width as f32 / metadata.height as f32) > 1.0 {
+        Stanice::BROD
+    } else {
+        Stanice::STAVIDLA
+    };
+
+
+    println!("{:?}", x);
 
 }
 
