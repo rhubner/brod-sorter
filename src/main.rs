8f0fd2b79ab14c21d91b9fe6e60e880ac50fa836
Radek Hubner <rhubner@users.noreply.github.com>
2021-06-17 10:15:20 +0100

    Detekce stanice a formatovani cesty.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 use std::path::Path;
+use std::path::PathBuf;
+use std::io;
+use std::io::Error;
+use std::io::ErrorKind;
+use regex::Regex;
 
 #[derive(Debug)]
 pub enum Stanice {
@@ -8,51 +13,81 @@ pub enum Stanice {
 
 
 fn main() {
-    use std::fs;
     use walkdir::WalkDir;
     use std::env;
-
     let args: Vec<String> = env::args().collect();
 
+    let re = Regex::new(r"^brod_(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})\.jpg$").unwrap();
+
     for entry in WalkDir::new(&args[1]).into_iter().filter_map(|e| e.ok())
-         .filter(|x| x.path().to_str().unwrap().ends_with("jpg") ) {
-         read_jpeg(&entry.path());
+        .filter(|x| x.path().to_str().unwrap().ends_with("jpg") ) {
+        let x = read_jpeg(&entry.path());
+        match x {
+            io::Result::Ok(Stanice::STAVIDLA) => {
+                let format = format_stavidla(&re, &args[2], &entry.path());
+                match format {
+                    Option::Some(path) => {
+                        println!("Prejnemovavam soubor : {}", path);
+                    }
+                    _ => println!("Nemohu formatovat cestu")
+                }
+            },
+            io::Result::Ok(Stanice::BROD) => println!("ignoruji, stanice brod"),
+            x => {
+                println!("jina stanice nez brod nebo chyba {:?}", x)
+            }
+        }
      }
 }
 
-fn read_jpeg(path: &Path) {
+
+fn copy_file(source: &Path, _dest: &str) {
+
+    let dest = Path::new(_dest);
+    let dest_dir = dest.parent().unwrap();
+    if !dest_dir.exists() {
+        std::fs::create_dir_all(dest_dir).unwrap();
+    }
+    std::fs::rename(source, &dest).unwrap();
+
+}
+
+fn format_stavidla(re: &Regex, path_prefix: &str, path: &Path) -> Option<String> {
+    let filename = path.file_name()?.to_str()?;
+    let capture_groups = re.captures(filename)?;
+    //println!("{:?}", capture_groups);
+
+    let stavidla_path = format!("{}/{}/{}/{}/brod-stavidla_{}{}{}{}{}.jpg", path_prefix,
+                                &capture_groups[1], &capture_groups[2], &capture_groups[3],
+                                &capture_groups[1], &capture_groups[2], &capture_groups[3], &capture_groups[4], &capture_groups[5]
+    );
+    //println!("{}", stavidla_path);
+
+    Option::Some(stavidla_path.to_string())
+}
+
+
+fn read_jpeg(path: &Path) -> io::Result<Stanice> {
     use std::fs::File;
     use std::io::BufReader;
+    use std::io::ErrorKind;
+
     use jpeg_decoder::Decoder;
 
-    let file = File::open(path).expect("failed to open file");
+    let file = File::open(path)?;
     let mut decoder = Decoder::new(BufReader::new(file));
-    decoder.read_info();
-    let metadata = decoder.info().unwrap();
+    let _ = decoder.read_info().map_err(map_read_error)?;
+    let metadata = decoder.info().ok_or(Error::new(ErrorKind::Other, "Unable to get info"))?;
 
-    let x = if (metadata.width as f32 / metadata.height as f32) > 1.0 {
-        Stanice::BROD
+    if (metadata.width as f32 / metadata.height as f32) > 1.0 {
+        io::Result::Ok(Stanice::BROD)
     } else {
-        Stanice::STAVIDLA
-    };
-
-
-    println!("{:?}", x);
-
-}
-
-fn show_focal_length(path: &str) -> Option<i32> {
-    use exif::{In, Tag};
-    let file = std::fs::File::open(path).ok()?;
-    let mut bufreader = std::io::BufReader::new(&file);
-    let exifreader = exif::Reader::new();
-    let exif = exifreader.read_from_container(&mut bufreader).ok()?;
-
-    let field = exif.get_field(Tag::FocalLengthIn35mmFilm, In::PRIMARY);
-
-    for f in field {
-        let ggg = f.value.get_uint(0).unwrap();
-        println!("{}", ggg);
+        io::Result::Ok(Stanice::STAVIDLA)
     }
-    Option::Some(10)
+
 }
+
+fn map_read_error(err: jpeg_decoder::Error) -> std::io::Error {
+    Error::new(ErrorKind::Other, format!("Nemohu precist ifo JPEG {}", err))
+}
+
